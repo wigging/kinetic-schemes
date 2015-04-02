ef49ed0eab7899c0d132bcc7b6a3c269941e666c
wigging <wigging@me.com>
2015-04-02 17:32:31 -0400

    updated Ranzi cell, hemi functions

--- funcRanzi.py
+++ funcRanzi.py
@@ -67,43 +67,61 @@ def cell(rhow, wt, T, dt, nt):
     # vector for initial wood concentration, kg/m^3
     pw = np.ones(nt)*rhow
     
-    # vector of initial cellulose concentration in wood, kg/m^3
-    cell = pw*(wt/100)
-    
     # vectors to store product concentrations, kg/m^3
-    cella = np.zeros(nt)
-    g1 = np.zeros(nt)
-    g2 = np.zeros(nt)
-    lvg = np.zeros(nt)
+    cell = pw*(wt/100)      # initial cellulose conc. in wood
+    g1 = np.zeros(nt)       # G1
+    cella = np.zeros(nt)    # CELLA
+    lvg = np.zeros(nt)      # LVG
+    g4 = np.zeros(nt)       # G4
     
     R = 1.987   # universal gas constant, kcal/kmol*K
     
     # A = pre-factor (1/s) and E = activation energy (kcal/kmol)
-    A1 = 4e13;  E1 = 45000  # CELL -> CELLA
-    A2 = 0.5e9; E2 = 29000  # CELLA -> G2
-    A3 = 1.8;   E3 = 10000  # CELLA -> LVG
-    A4 = 4e7;   E4 = 31000  # CELL -> G1
+    A1 = 4e7;   E1 = 31000      # CELL -> G1
+    A2 = 4e13;  E2 = 45000      # CELL -> CELLA
+    A3 = 1.8*T;   E3 = 10000    # CELLA -> LVG
+    A4 = 0.5e9; E4 = 29000      # CELLA -> G4
     
     # reaction rate constant for each reaction, 1/s
-    K1 = A1 * np.exp(-E1 / (R * T))         # CELL -> CELLA
-    K2 = A2 * np.exp(-E2 / (R * T))         # CELLA -> G2
-    K3 = A3 * T * np.exp(-E3 / (R * T))     # CELLA -> LVG
-    K4 = A4 * np.exp(-E4 / (R * T))         # CELL -> G1
+    K1 = A1 * np.exp(-E1 / (R * T))     # CELL -> G1
+    K2 = A2 * np.exp(-E2 / (R * T))     # CELL -> CELLA
+    K3 = A3 * np.exp(-E3 / (R * T))     # CELLA -> LVG
+    K4 = A4 * np.exp(-E4 / (R * T))     # CELLA -> G4
     
     # calculate concentrations for each product, kg/m^3
     for i in range(1, nt):
-        r1 = K1 * cell[i-1]
-        r2 = K2 * cella[i-1]
-        r3 = K3 * cella[i-1]
-        r4 = K4 * cell[i-1]
-        cell[i] = cell[i-1] - (r1+r4)*dt            # CELL
-        cella[i] = cella[i-1] + r1*dt - (r2+r3)*dt  # CELLA
-        g1[i] = g1[i-1] + r4*dt                     # G1
-        g2[i] = g2[i-1] + r2*dt                     # G2
+        r1 = K1 * cell[i-1]     # CELL -> G1
+        r2 = K2 * cell[i-1]     # CELL -> CELLA
+        r3 = K3 * cella[i-1]    # CELLA -> LVG
+        r4 = K4 * cella[i-1]    # CELLA -> G4
+        cell[i] = cell[i-1] - (r1+r2)*dt            # CELL
+        g1[i] = g1[i-1] + r1*dt                     # G1
+        cella[i] = cella[i-1] + r2*dt - (r3+r4)*dt  # CELLA
         lvg[i] = lvg[i-1] + r3*dt                   # LVG
+        g4[i] = g4[i-1] + r4*dt                     # G4
+    
+    sumg1 = 11      # sum of G1 species
+    sumg4 = 4.08    # sum of G4 species
+    
+    h2o = 5*g1/sumg1 + 0.83*g4/sumg4    # H2O
+    char = 6*g1/sumg1 + 0.61*g4/sumg4   # Char
+    haa = 0.8*g4/sumg4                  # HAA
+    glyox = 0.2*g4/sumg4                # Glyox
+    c2h4o = 0.1*g4/sumg4                # C2H4O
+    hmfu = 0.25*g4/sumg4                # HMFU
+    c3h6o = 0.3*g4/sumg4                # C3H6O
+    co2 = 0.21*g4/sumg4                 # CO2
+    h2 = 0.1*g4/sumg4                   # H2
+    ch2o = 0.4*g4/sumg4                 # CH2O
+    co = 0.16*g4/sumg4                  # CO
+    ch4 = 0.1*g4/sumg4                  # CH4
+    hcooh = 0.02*g4/sumg4               # HCOOH
     
     # return array of concentrations as a density, kg/m^3
-    return np.array([cell, cella, g1, g2, lvg])
+    prod = np.array([cell, cella, g1, g4, lvg, h2o, char, haa, glyox, c2h4o, \
+                    hmfu, c3h6o, co2, h2, ch2o, co, ch4, hcooh])
+    
+    return prod
     
     
 # Function - hemicellulose reactions, HCE
@@ -121,15 +139,13 @@ def hce(rhow, wt, T, dt, nt):
     # vector for initial wood concentration, kg/m^3
     pw = np.ones(nt)*rhow
     
-    # vector of initial hemicellulose concentration in wood, kg/m^3
-    hce = pw*(wt/100)
-    
     # vectors to store product concentrations, kg/m^3
-    g1 = np.zeros(nt)
-    g2 = np.zeros(nt)
-    g3 = np.zeros(nt)
-    g4 = np.zeros(nt)
-    xyl = np.zeros(nt)
+    hce = pw*(wt/100)   # initial hemicellulose conc. in wood
+    g1 = np.zeros(nt)   # G1
+    g2 = np.zeros(nt)   # G2
+    g3 = np.zeros(nt)   # G3
+    g4 = np.zeros(nt)   # G4
+    xyl = np.zeros(nt)  # Xylan
     
     R = 1.987   # universal gas constant, kcal/kmol*K
     
@@ -148,21 +164,50 @@ def hce(rhow, wt, T, dt, nt):
     K5 = A5 * np.exp(-E5 / (R * T))     # HCE1 -> Xylan
     
     # calculate concentrations for each product, kg/m^3
+    # where HCE1 as 0.4*g1/(0.4+0.6) and HCE2 as 0.6*g1/(0.4+0.6)
     for i in range(1, nt):
-        r1 = K1 * hce[i-1]
-        r2 = K2 * 0.6*g1[i-1]   # from HCE2 as 0.6*g1/(0.4+0.6)
-        r3 = K3 * 0.4*g1[i-1]   # from HCE1 as 0.4*g1/(0.4+0.6)
-        r4 = K4 * 0.4*g1[i-1]   # also from HCE1
-        r5 = K5 * 0.4*g1[i-1]   # also from HCE1
-        hce[i] = hce[i-1] - r1*dt                 # HCE
+        r1 = K1 * hce[i-1]      # HCE -> G1
+        r2 = K2 * 0.6*g1[i-1]   # HCE2 -> G2
+        r3 = K3 * 0.4*g1[i-1]   # HCE1 -> G3
+        r4 = K4 * 0.4*g1[i-1]   # HCE1 -> G4
+        r5 = K5 * 0.4*g1[i-1]   # HCE1 -> Xylan
+        hce[i] = hce[i-1] - r1*dt                   # HCE
         g1[i] = g1[i-1] + r1*dt - (r2+r3+r4+r5)*dt  # G1
         g2[i] = g2[i-1] + r2*dt                     # G2
         g3[i] = g3[i-1] + r3*dt                     # G3
         g4[i] = g4[i-1] + r4*dt                     # G4
         xyl[i] = xyl[i-1] + r5*dt                   # Xylan
         
+    # sum of moles in each group, mol
+    sumg2 = 4.625   # sum of G2
+    sumg3 = 4.875   # sum of G3
+    sumg4 = 4.775   # sum of G4
+    
+    # group concentration per total moles in that group, (kg/m^3)/mol
+    fg2 = g2/sumg2  # fraction of G2 
+    fg3 = g3/sumg3  # fraction of G3
+    fg4 = g4/sumg4  # fraction of G4
+    
+    prod = np.zeros([21, nt]) # array to store products
+    
+    prod[0] = 0.175*fg2 + (0.3 + 0.15)*fg3 + 0.5*fg4                # CO
+    prod[1] = (0.275+0.4)*fg2 + (0.5+0.25)*fg3 + (0.5+0.275)*fg4    # CO2
+    prod[2] = (0.5+0.925)*fg2 + 1.7*fg3 + (0.8+0.4)*fg4             # CH2O
+    prod[3] = 0.025*fg2 + 0.05*fg3 + 0.025*fg4                      # HCOOH
+    prod[4] = 0.3*fg2 + (0.1+0.45)*fg4                              # CH3OH
+    prod[5] = 0.25*fg2 + 0.625*fg3 + 0.325*fg4                      # CH4
+    prod[7] = 0.275*fg2 + 0.375*fg3 + 0.25*fg4                      # C2H4
+    prod[9] = 0.2*fg2                                               # HAA
+    prod[10] = 0.1*fg2 + 0.125*fg4                                  # C2H5OH
+    prod[12] = xyl                                                  # Xylan
+    prod[18] = 0.125*fg4                                            # H2
+    prod[19] = 0.2*fg2 + 0.25*fg3 + 0.025*fg4                       # H2O
+    prod[20] = 1*fg2 + 0.675*fg3 + 0.875*fg4                        # Char
+    
     # return array of concentrations as a density, kg/m^3
-    return np.array([hce, g1, g2, g3, g4, xyl])
+    prod = np.array([hce, g1, g2, g3, g4, xyl])
+    
+    return prod
     
     
 # Function - lignin carbon-rich reactions, LIG-C
--- plot_funcRanzi.py
+++ plot_funcRanzi.py
@@ -30,27 +30,39 @@ tmax = 4                                # max time, s
 t = np.linspace(0, tmax, num=tmax/dt)   # time vector
 nt = len(t)                             # total number of time steps
 
-# Calculate Products from Cellulose Reactions
+# Products from Cellulose, Hemicellulose, Lignin Reactions
 #------------------------------------------------------------------------------
 
 # array of product concentrations as a density, kg/m^3
 pcell = kn.cell(rhow, wtcell, Tinf, dt, nt)
 
-# convert product concentrations to mass fraction, (-)    
+# products as mass fraction, (-)    
 cell = pcell[0]/rhow    # CELL
 cella = pcell[1]/rhow   # CELLA
 g1 = pcell[2]/rhow      # G1
-g2 = pcell[3]/rhow      # G2
+g4 = pcell[3]/rhow      # G4
 lvg = pcell[4]/rhow     # LVG
+h2o = pcell[5]/rhow     # H2O
+char = pcell[6]/rhow    # Char
+haa = pcell[7]/rhow     # HAA
+glyox = pcell[8]/rhow   # Glyox
+c2h4o = pcell[9]/rhow   # C2H4O
+hmfu = pcell[10]/rhow   # HMFU
+c3h6o = pcell[11]/rhow  # C3H6O
+co2 = pcell[12]/rhow    # CO2
+h2 = pcell[13]/rhow     # H2
+ch2o = pcell[14]/rhow   # CH2O
+co = pcell[15]/rhow     # CO
+ch4 = pcell[16]/rhow    # CH4
+hcooh = pcell[17]/rhow  # HCOOH
 
-total = cell + cella + g1 + g2 + lvg # mass balance for CELL, CELLA, G1, G2, LVG
+# mass balances
+total = cell + cella + g1 + g4 + lvg # for CELL, CELLA, G1, G$, LVG
 
-h2o = 5*g1/(5+6)            # H2O
-char = 6*g1/(5+6)           # Char
+total2 = cell + cella + lvg + h2o + char + haa + glyox + c2h4o + hmfu + c3h6o + co2 + \
+         h2 + ch2o + co + ch4 + hcooh  # approach 2
 
-total2 = cell+cella+h2o+char+g2+lvg # check mass balance again
-
-# Calculate Products from Hemicellulose Reactions
+# Products from Hemicellulose Reactions
 #------------------------------------------------------------------------------
 
 # array of product concentrations as a density, kg/m^3
@@ -64,7 +76,7 @@ g33 = phce[3]/rhow
 g44 = phce[4]/rhow
 xyl = phce[5]/rhow
 
-# Calculate Products from Lignin Reactions
+# Products from Lignin Reactions
 #------------------------------------------------------------------------------
 
 # array of product concentrations as a density, kg/m^3
@@ -87,35 +99,54 @@ py.figure(1)
 py.plot(t, cell, label='cell')
 py.plot(t, cella, label='cella')
 py.plot(t, g1, label='g1')
-py.plot(t, g2, label='g2')
+py.plot(t, g4, label='g4')
 py.plot(t, lvg, label='lvg')
 py.plot(t, total, label='total')
-py.plot(t, h2o, label='h2o')
-py.plot(t, char, label='char')
 py.title('Cellulose Reactions at T = {} K'.format(Tinf))
 py.xlabel('Time (s)')
 py.ylabel('Mass Fraction (dry basis)')
 py.legend(loc='best', numpoints=1)
 
 py.figure(2)
-py.plot(t, hce, label='hce')
-py.plot(t, g11, label='g1')
-py.plot(t, g22, label='g2')
-py.plot(t, g33, label='g3')
-py.plot(t, g44, label='g4')
-py.plot(t, xyl, label='xyl')
-py.title('Hemicellulose Reactions at T = {} K'.format(Tinf))
+py.plot(t, h2o, label='$\mathrm{H_{2}O}$')
+py.plot(t, char, label='char')
+py.plot(t, haa, label='haa')
+py.plot(t, glyox, label='glyox')
+py.plot(t, c2h4o, label='$\mathrm{C_{2}H_{4}O}$')
+py.plot(t, hmfu, label='HMFU')
+py.plot(t, c3h6o, label='$\mathrm{C_{3}H_{6}O}$')
+py.plot(t, co2, ls='--', label='$\mathrm{CO_2}$')
+py.plot(t, h2, ls='--', label='$\mathrm{H_2}$')
+py.plot(t, ch2o, ls='--', label='$\mathrm{CH_2O}$')
+py.plot(t, co, ls='--', label='$\mathrm{CO}$')
+py.plot(t, ch4, ls='--', label='$\mathrm{CH_4}$')
+py.plot(t, hcooh, ls='--', label='$\mathrm{HCOOH}$')
+py.plot(t, lvg, ls='--', label='lvg')
+py.plot(t, total2, ls='--', label='total2')
+py.title('Cellulose Reactions at T = {} K'.format(Tinf))
 py.xlabel('Time (s)')
 py.ylabel('Mass Fraction (dry basis)')
 py.legend(loc='best', numpoints=1)
 
-py.figure(3)
-py.plot(t, ligc, label='ligc')
-py.plot(t, g111, label='g1')
-py.plot(t, g222, label='g2')
-py.title('Lignin Reactions at T = {} K'.format(Tinf))
-py.xlabel('Time (s)')
-py.ylabel('Mass Fraction (dry basis)')
-py.legend(loc='best', numpoints=1)
+#py.figure(2)
+#py.plot(t, hce, label='hce')
+#py.plot(t, g11, label='g1')
+#py.plot(t, g22, label='g2')
+#py.plot(t, g33, label='g3')
+#py.plot(t, g44, label='g4')
+#py.plot(t, xyl, label='xyl')
+#py.title('Hemicellulose Reactions at T = {} K'.format(Tinf))
+#py.xlabel('Time (s)')
+#py.ylabel('Mass Fraction (dry basis)')
+#py.legend(loc='best', numpoints=1)
+#
+#py.figure(3)
+#py.plot(t, ligc, label='ligc')
+#py.plot(t, g111, label='g1')
+#py.plot(t, g222, label='g2')
+#py.title('Lignin Reactions at T = {} K'.format(Tinf))
+#py.xlabel('Time (s)')
+#py.ylabel('Mass Fraction (dry basis)')
+#py.legend(loc='best', numpoints=1)
 
 py.show()
